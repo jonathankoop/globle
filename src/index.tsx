fc6bcd82a9471fe5148cda84a0cedcd310c83386
jonathankoop <j.koop@uu.nl>
2025-02-04 15:53:38 +0100

    Fix GitHub Pages routing with basename

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
       <LocaleProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/globle">
           <App />
           <BodyStyle />
         </BrowserRouter>
